b2b201e66678f4437c95ee2486ddddfaad60d661
Andrew Robbins <hello@wpshop.io>
2022-04-19 12:36:18 -0500

    Updated docs

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -1,19 +1,16 @@
 import React from 'react'
 
 function Highlight({ children, color }) {
-	const Shop = shopwpComponents.Shop
 	return (
-		<Shop>
-			<span
-				style={{
-					backgroundColor: color,
-					borderRadius: '2px',
-					color: '#fff',
-					padding: '0.2rem',
-				}}>
-				{children}
-			</span>
-		</Shop>
+		<span
+			style={{
+				backgroundColor: color,
+				borderRadius: '2px',
+				color: '#fff',
+				padding: '0.2rem',
+			}}>
+			{children}
+		</span>
 	)
 }
 
