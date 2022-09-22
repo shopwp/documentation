416e681b9827e57b1f0be7a94a4abf00062321a8
Andrew Robbins <hello@wpshop.io>
2022-09-22 12:28:22 -0500

    6.1.2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ module.exports = {
 				items: [
 					{
 						value:
-							'<span style="margin-left:-20px;">Latest release: v6.1.1</span>',
+							'<span style="margin-left:-20px;">Latest release: v6.1.2</span>',
 						type: 'html',
 						position: 'left',
 					},
