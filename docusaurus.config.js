c38b49f5d0a2ee5e78886d6ed62c88048078c5ba
Andrew Robbins <hello@wpshop.io>
2023-01-20 05:06:32 -0600

    Release: 6.3.6

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ module.exports = {
 				},
 				items: [
 					{
-						value: '<span class="version-num">v6.3.5</span>',
+						value: '<span class="version-num">v6.3.6</span>',
 						type: 'html',
 						position: 'left',
 					},
