c59d71567b9ae12b2f6ed92f54a499a992415365
Andrew Robbins <hello@wpshop.io>
2023-05-04 22:06:47 -0500

    Release: 7.1.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ module.exports = {
 				items: [
 					{
 						value:
-							'<p class="version-num"><span class="num">v7.1.0</span><span class="emoji">🎉</span></p>',
+							'<p class="version-num"><span class="num">v7.1.1</span><span class="emoji">🎉</span></p>',
 						type: 'html',
 						position: 'left',
 					},
