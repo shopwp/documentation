a1efd3d8624a9812ca252de8587afecd41da2fa3
Andrew Robbins <andrew.robbins@wpengine.com>
2025-10-06 12:05:38 -0500

    Bumped version number

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,7 @@ module.exports = {
 			items: [
 				{
 					value:
-						'<p class="version-num"><span class="num">v8.11.1</span><span class="emoji">🎉</span></p>',
+						'<p class="version-num"><span class="num">v8.11.3</span><span class="emoji">🎉</span></p>',
 					type: 'html',
 					position: 'left',
 				},
